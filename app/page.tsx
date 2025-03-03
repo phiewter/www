c3916b97d865713b847956dc9ee9a03111293e4d
phiewter <19486709+phiewter@users.noreply.github.com>
2025-03-03 21:26:38 +0100

    feat: add static metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+import { Metadata } from "next";
+
 import { Container } from "@/app/components/container/container";
 import { Header } from "@/app/components/header/header";
 import { Page } from "@/app//components/page/page";
+
+export const metadata: Metadata = {
+  title: "Pieter van Arend's blog",
+  description:
+    "This is my personal website, where I share projects, ideas, and things I'm exploring. Feel free to look around and see what I'm working on.",
+};
 
 export default function Home() {
   return (
