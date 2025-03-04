c83f81ef525603a984903830772aa0b44b41f46d
phiewter <19486709+phiewter@users.noreply.github.com>
2025-03-04 22:47:57 +0100

    feat: add feature-card

---
+++ app/components/feature-card/feature-card.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import * as React from "react";
+
+import clsx from "clsx";
+
+import Image from "next/image";
+
+import styles from "@/app/components/feature-card/feature-card.module.css";
+
+interface FeatureCardProps {
+  children: React.ReactNode;
+}
+
+const FeatureCard: React.FC<FeatureCardProps> & {
+  Image: React.FC<FeatureCardImageProps>;
+  Title: React.FC<FeatureCardTitleProps>;
+  Description: React.FC<FeatureCardDescriptionProps>;
+} = ({ children }) => {
+  return <div className={clsx(styles["feature-card"])}>{children}</div>;
+};
+FeatureCard.displayName = "FeatureCard";
+
+interface FeatureCardImageProps {
+  src: string;
+  alt: string;
+}
+
+FeatureCard.Image = ({ src, alt }) => {
+  return (
+    <Image
+      className={styles.image}
+      width={64}
+      height={64}
+      src={src}
+      alt={alt}
+    />
+  );
+};
+FeatureCard.Image.displayName = "FeatureCardImage";
+
+interface FeatureCardTitleProps {
+  children: React.ReactNode;
+}
+
+FeatureCard.Title = ({ children }) => {
+  return <h2 className={clsx(styles.title)}>{children}</h2>;
+};
+FeatureCard.Title.displayName = "FeatureCardTitle";
+
+interface FeatureCardDescriptionProps {
+  children: React.ReactNode;
+}
+
+FeatureCard.Description = ({ children }) => {
+  return <p className={clsx(styles.description)}>{children}</p>;
+};
+FeatureCard.Description.displayName = "FeatureCardDescription";
+
+// Pre-defined function [Derivative of <FeatureCard>]
+const _FeatureCard: React.FC = () => {
+  return (
+    <FeatureCard>
+      <FeatureCard.Image src="/avatar.jpg" alt="GitHub Avatar" />
+      <div className={styles["text-wrapper"]}>
+        <FeatureCard.Title>This is a test title.</FeatureCard.Title>
+        <FeatureCard.Description>Description</FeatureCard.Description>
+      </div>
+    </FeatureCard>
+  );
+};
+
+export { _FeatureCard };
